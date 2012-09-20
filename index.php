8ae54a7b40c421af5426149e2be4b862883ee0b1
Alex Lindeman <aelindeman@gmail.com>
2012-09-20 19:09:41 +0000

    added iPhone loading screen and high-res, font-less Ship logo to extras folder

--- index.php
+++ index.php
@@ -121,6 +121,7 @@ function show_nonfatal_errors ($errors, $config)
 		<meta name="apple-mobile-web-app-capable" content="yes" />
 		<meta name="apple-mobile-web-app-status-bar-style" content="black" />
 		<link rel="apple-touch-icon" href="./img/iphone-icon.png" />
+		<link rel="apple-touch-startup-image" href="./img/default.png" />
 		<meta name="format-detection" content="telephone=no" />
 		<meta name="viewport" content="width=420, initial-scale=0.75, user-scalable=no" />
 
